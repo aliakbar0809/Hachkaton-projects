dda70892c083058576825610e53fac6e306ba624
Aliakbar <aliakbarsherov266@gmail.com>
2025-12-07 14:27:05 +0300

    Convertator

--- currency/views.py
+++ currency/views.py
@@ -10,19 +10,29 @@ class DailyCurrencyRatesView(APIView):
     @swagger_auto_schema(
         manual_parameters=[
             openapi.Parameter(
+                'base',
+                openapi.IN_QUERY,
+                description="Базовая валюта (tjs, usd, eur, rub)",
+                type=openapi.TYPE_STRING,
+                required=False,
+                default="tjs"
+            ),
+            openapi.Parameter(
                 'amount',
                 openapi.IN_QUERY,
-                description="Сумма в TJS",
+                description="Сумма в базовой валюте",
                 type=openapi.TYPE_NUMBER,
-                required=False
+                required=False,
+                default=1
             ),
         ],
         responses={200: "OK"}
     )
     def get(self, request):
+      
+        base = request.GET.get("base", "tjs").lower()
         amount = float(request.GET.get("amount", 1))
 
-        base = "tjs"
         url = f"https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/{base}.json"
 
         try:
@@ -34,17 +44,20 @@ class DailyCurrencyRatesView(APIView):
             if not rates:
                 return Response({"error": "Нет курсов для базовой валюты", "raw": data}, status=500)
 
-            
-            filtered = {}
-            for k in ["usd", "eur", "rub", "tjs"]:
+           
+            target_currencies = ["tjs", "usd", "eur", "rub"]
+
+          
+            converted = {}
+            for k in target_currencies:
                 if k in rates:
-                    filtered[k.upper()] = round(amount * rates[k], 2)
+                    converted[k.upper()] = round(amount * rates[k], 2)
 
             return Response({
                 "base": base.upper(),
                 "amount": amount,
                 "date": data.get("date"),
-                "converted": filtered
+                "converted": converted
             })
 
         except Exception as e:
